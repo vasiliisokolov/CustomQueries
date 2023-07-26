170bb3ac9beabd548a136a3486da95bc4214cde7
Vasily Sokolov <vasilysokolov@mail.ru>
2023-07-26 21:24:29 +0300

    interface

--- CustomQueries.cpp
+++ CustomQueries.cpp
@@ -1,6 +1,32 @@
 ﻿#include "CustomQueries.h"
 #include <cpr/cpr.h>
 
+class UserInterface
+{
+public:
+
+	void getCPR()
+	{
+		cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+		std::cout << r.text << std::endl;
+	}
+
+	void postCPR()
+	{
+
+	}
+
+	void putCPR()
+	{
+
+	}
+
+	void delCPR()
+	{
+
+	}
+};
+
 int main()
 {
 	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
