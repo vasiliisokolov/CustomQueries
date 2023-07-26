0b7c60d05e27e3f1eb10693774f605893aeb99d8
Vasily Sokolov <vasilysokolov@mail.ru>
2023-07-26 21:18:37 +0300

    get ready

--- CustomQueries.cpp
+++ CustomQueries.cpp
@@ -1,10 +1,8 @@
 ﻿#include "CustomQueries.h"
 #include <cpr/cpr.h>
 
-using namespace std;
-
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+	std::cout << r.text << std::endl;
 }
