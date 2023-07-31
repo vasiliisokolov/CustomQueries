fefae1a8e9da2de87f11650e61fe5dfa71008b98
Vasily Sokolov <vasilysokolov@mail.ru>
2023-07-31 17:30:34 +0300

    Done!

--- CustomQueries.cpp
+++ CustomQueries.cpp
@@ -9,38 +9,85 @@ public:
 
 	void getCPR()
 	{
-		r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+		std::cout << "Get!" << std::endl;
+
+		r = cpr::Get(cpr::Url{"http://httpbin.org/get"});
 		
 	}
 
 	void postCPR()
 	{
-		r = cpr::Post(cpr::Url("http://httpbin.org/post"));
+		std::cout << "Post!" << std::endl;
+		r = cpr::Post(cpr::Url{"http://httpbin.org/post"}, cpr::Payload{{"adress", "square"}});
 	}
 
 	void putCPR()
 	{
-		r = cpr::Put(cpr::Url("http://httpbin.org/put"));
+		std::cout << "Put!" << std::endl;
+		r = cpr::Put(cpr::Url{"http://httpbin.org/put"}, cpr::Payload{{"adressA", "squareA"}});
 	}
 
 	void delCPR()
 	{
-		r = cpr::Delete(cpr::Url("http://httpbin.org/delete"));
+		std::cout << "Delete!" << std::endl;
+		r = cpr::Delete(cpr::Url{"http://httpbin.org/get"});
 	}
 	
 	void patchCPR()
 	{
-		r = cpr::Patch(cpr::Url("http://httpbin.org/patch"));
+		std::cout << "Patch!" << std::endl;
+		r = cpr::Patch(cpr::Url{"http://httpbin.org/patch"}, cpr::Payload{{"adressB", "squareB"}});
 	}
 
 	void out()
 	{
 		std::cout << r.text << std::endl;
 	}
+
+	void run(int operation)
+	{
+		if (operation > 5) operation = 5;
+		else if (operation < 1) operation = 1;
+
+		switch (operation)
+		{
+		case 1:
+			getCPR();
+			out();
+			break;
+		case 2:
+			postCPR();
+			out();
+			break;
+		case 3:
+			putCPR();
+			out();
+			break;
+		case 4:
+			delCPR();
+			out();
+			break;
+		case 5:
+			patchCPR();
+			out();
+			break;
+		}
+	}
 };
 
 int main()
 {
-	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
-	std::cout << r.text << std::endl;
+	UserInterface inter;
+	int operation;
+	while (true)
+	{
+		std::cout << "Choose the operation: ";
+		std::cin >> operation;
+
+		if (operation == 0) return 0;
+		else
+		{
+			inter.run(operation);
+		}
+	}
 }
